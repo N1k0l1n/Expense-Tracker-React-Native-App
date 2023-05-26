ac5843eb75708bdfe305c148ce8785ff2b146fb6
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-05-26 15:40:40 +0200

    styling items

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,8 +1,9 @@
 import { FlatList, Text } from "react-native";
 import React from "react";
+import ExpenseItem from "./ExpenseItem";
 
 function renderExpenseItem(itemData){
-    return <Text>{itemData.item.description}</Text>
+    return <ExpenseItem {...itemData.item}/>
 }
 
 export default function ExpensesList({expenses}) {
--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,7 +1,8 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import React from "react";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
 
 const DUMMY_EXPENSES = [
   {
@@ -34,13 +35,53 @@ const DUMMY_EXPENSES = [
     amount: 10.29,
     date: new Date('2022-03-12')
   },
+  {
+    id: "e6",
+    description: "A pair of Shoes",
+    amount: 59.99,
+    date: new Date('2022-12-23')
+  },
+  {
+    id: "e7",
+    description: "A pair of CLothes",
+    amount: 89.29,
+    date: new Date('2022-01-05')
+  },
+  {
+    id: "e8",
+    description: "A pair of things",
+    amount: 9.29,
+    date: new Date('2022-11-05')
+  },
+  {
+    id: "e9",
+    description: "A book",
+    amount: 19.29,
+    date: new Date('2022-08-06')
+  },
+  {
+    id: "e10",
+    description: "A thing",
+    amount: 10.29,
+    date: new Date('2022-03-12')
+  },
 ];
 
 export default function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList expenses={DUMMY_EXPENSES}/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        paddingTop:24,
+        paddingHorizontal:24,
+        paddingBottom:0,
+        backgroundColor:GlobalStyles.colors.primary700
+    }
+})
